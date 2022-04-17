dab381d27934490d538581cd07ed2243b2313c1c
BilalMemon010 <94567372+BilalMemon010@users.noreply.github.com>
2022-04-17 20:30:23 +0500

    Add files via upload

---
+++ main/Main.java
@@ -0,0 +1,11 @@
+
+package edu.gdse41.sem1project.main;
+
+import edu.gdse41.sem1project.view.SplashScreen;
+
+
+public class Main {
+    public static void main(String[] args) {
+        new SplashScreen().setVisible(true);
+    }
+}
